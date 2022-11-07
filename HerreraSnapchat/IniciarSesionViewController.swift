2f14d172910594bbbff34ff01655054ab0011039
Yair Herrera <yair.herrera@tecsup.edu.pe>
2022-11-06 21:04:19 -0500

    Fase Final

---
+++ HerreraSnapchat/IniciarSesionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  IniciarSesionViewController.swift
+//  HerreraSnapchat
+//
+//  Created by Yair Herrera on 31/10/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class IniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil {
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
